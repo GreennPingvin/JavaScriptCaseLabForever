e0a64aa987b921a1fb11cb66f8c3bc90753e0936
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-09-05 09:45:52 +0300

    change package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,89 +10,89 @@ const target = isDev ? "web" : "browserslist";
 const devtool = isDev ? "source-map" : undefined;
 
 const getFileName = (ext) =>
-  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+	isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
-  mode,
-  target,
-  devtool,
-  entry: path.resolve(__dirname, "src/js/index.js"),
-  output: {
-    filename: getFileName("js"),
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[hash][ext][query]",
-    clean: true,
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash].css",
-    }),
-    // new CopyPlugin({
-    //   patterns: [{ from: "src/assets", to: "assets" }],
-    // }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(c|sc|sa)ss$/i,
-        use: [
-          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    require("postcss-preset-env"),
-                    {
-                      // Options
-                    },
-                  ],
-                ],
-              },
-            },
-          },
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|jpg)/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2)/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[hash][ext][query]",
-        },
-      },
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
-          },
-        },
-      },
-    ],
-  },
-  stats: {
-    children: true,
-  },
+	mode,
+	target,
+	devtool,
+	entry : path.resolve(__dirname, "src/js/index.js"),
+	output : {
+		filename : getFileName("js"),
+		path : path.resolve(__dirname, "dist"),
+		assetModuleFilename : "images/[hash][ext][query]",
+		clean : true,
+	},
+	devServer : {
+		port : 3000,
+		open : true,
+		hot : true,
+	},
+	plugins : [
+		new HtmlWebpackPlugin({
+			template : path.resolve(__dirname, "src/index.html"),
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename : "css/[name].[contenthash].css",
+		}),
+		new CopyPlugin({
+			patterns : [{ from : "src/assets", to : "assets" }],
+		}),
+	],
+	module : {
+		rules : [
+			{
+				test : /\.html$/i,
+				loader : "html-loader",
+			},
+			{
+				test : /\.(c|sc|sa)ss$/i,
+				use : [
+					isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader : "postcss-loader",
+						options : {
+							postcssOptions : {
+								plugins : [
+									[
+										require("postcss-preset-env"),
+										{
+											// Options
+										},
+									],
+								],
+							},
+						},
+					},
+					"sass-loader",
+				],
+			},
+			{
+				test : /\.(png|jpg)/,
+				type : "asset/resource",
+			},
+			{
+				test : /\.(woff|woff2|ttf)/,
+				type : "asset/resource",
+				generator : {
+					filename : "fonts/[hash][ext][query]",
+				},
+			},
+			{
+				test : /\.(?:js|mjs|cjs)$/,
+				exclude : /node_modules/,
+				use : {
+					loader : "babel-loader",
+					options : {
+						presets : [["@babel/preset-env", { targets : "defaults" }]],
+					},
+				},
+			},
+		],
+	},
+	stats : {
+		children : true,
+	},
 };
